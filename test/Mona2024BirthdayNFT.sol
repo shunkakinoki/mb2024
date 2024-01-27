f5147fb1b6f679b2e955ab3a6468de93d29bcb04
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-27 14:47:51 -0800

    chore: update

--- test/Mona2024BirthdayNFT.sol
+++ test/Mona2024BirthdayNFT.sol
@@ -8,50 +8,79 @@ contract Mona2024BirthdayNFTTest is Test {
     Mona2024BirthdayNFT nft;
 
     function setUp() public {
+        // Deploy the Mona2024BirthdayNFT contract
         nft = new Mona2024BirthdayNFT();
     }
 
-    function testInitialNFTOwner() public {
+    function testNFTOwner() public {
+        // Check the initial owner of the NFT
         assertEq(nft.ownerOf(0), address(0x2aF8DDAb77A7c90a38CF26F29763365D0028cfEf), "NFT should be initially owned by Mona's address");
     }
 
-    function testTokenName() public {
+    function testName() public {
+        // Get the name of the token
         string memory name = nft.name();
+        
+        // Check if the name is not empty
         assertEq(name, "Mona 2024 Birthday NFT", "Token name does not match expected.");
     }
 
-    function testTokenSymbol() public {
+    function testSymbol() public {
+        // Get the symbol of the token
         string memory symbol = nft.symbol();
+        
+        // Check if the symbol is not empty
         assertEq(symbol, "MB2024", "Token symbol does not match expected.");
     }
 
-    function testTokenDescription() public {
+    function testDescription() public {
+        // Get the description of the token
         string memory description = nft.description();
-        // Replace with actual expected description
+        
+        // Check if the description is not empty
         assertTrue(bytes(description).length> 0, "Token description does not match expected.");
     }
 
     function testDeposit() public {
+        // Check the initial balance of the contract
         uint initialBalance = address(this).balance;
+        
+        // Deposit 1 Ether
         nft.deposit{value: 1 ether}();
+        
+        // Check the new balance of the contract after depositing
         assertEq(address(this).balance, initialBalance - 1 ether, "balance should decrease by 1 ether after depositing");
         assertEq(address(nft).balance, 1 ether, "balance of address(this) should be 1 ether");
     }
 
     function testFailedWithdraw() public {
+        // Deposit 1 Ether
+        nft.deposit{value: 1 ether}();
+
+        // Try to withdraw 1 Ether
         // vm.expectRevert();
         nft.withdraw(1 ether);
+
+        // Check the new balance of the contract after withdrawal
+        assertEq(address(nft).balance, 1, "Contract balance should be 1 after the attempted withdrawal");
     }
 
     function testWithdrawByMona() public {
+        // Deposit 1 Ether
         nft.deposit{value: 1 ether}();
+
+        // Prank Mona's address to make Mona appear as the transaction sender, then withdraw 1 Ether
         vm.prank(address(0x2aF8DDAb77A7c90a38CF26F29763365D0028cfEf));
         nft.withdraw(1 ether);
+
+        // Check the new balance of the contract after withdrawal
+        assertEq(address(nft).balance, 0, "Contract balance should be 0 after the withdrawal");
+
+        // Check the new balance of Mona's address after withdrawal
+        assertEq(address(0x2aF8DDAb77A7c90a38CF26F29763365D0028cfEf).balance, 1 ether, "Mona's balance should be 1 ether after the withdrawal");
     }
 
     function testMultipleDepositsAndCumulativeWithdrawals() public {
-        uint initialBalance = address(this).balance;
-
         // Deposit 1 Ether three times
         nft.deposit{value: 1 ether}();
         nft.deposit{value: 1 ether}();
