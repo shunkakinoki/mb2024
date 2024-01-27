39ef1e6c79187f69123b16ac77eb2ab33a454a5d
Shun Kakinoki <shunkakinoki@gmail.com>
2024-01-27 15:04:37 -0800

    chore: update

--- script/Mona2024BirthdayNFTDeployScript.s.sol
+++ script/Mona2024BirthdayNFTDeployScript.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 
-contract Mona2024BirthdayNFTScript is Script {
+contract Mona2024BirthdayNFTDeployScript is Script {
     function setUp() public {}
 
     function run() public {
